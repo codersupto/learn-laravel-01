5e4f11272fcbfe820423024505a21ad370a78999
Supto Purakayasto <codersupto@gmail.com>
2021-01-19 15:25:13 +0600

    ...

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        '/pay-via-ajax', '/success','/cancel','/fail','/ipn', 'http://localhost:4200/*'
     ];
 }
