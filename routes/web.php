4488b015e4a1394f3ddf3e1a93698aa95f2b25c7
Supto Purakayasto <codersupto@gmail.com>
2021-01-23 14:30:14 +0600

    ...

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return \Illuminate\Support\Facades\Mail::to('developersuptop@gmail.com')->send(new \App\Mail\SesMail());
 });
